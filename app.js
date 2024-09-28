eba49bc1c302909b7492b1d2328195dae81862c9
Waqar Ahmed <wwishking@gmail.com>
2024-09-28 13:09:30 +0500

    Update

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 "use strict";
-function toggleVisibility(sectionId) {
+function toggleVisibility2(sectionId) {
+    // Get the section by its ID
     const section = document.getElementById(sectionId);
+    // If the section exists, toggle the 'hidden' class
     if (section) {
         section.classList.toggle('hidden');
     }
